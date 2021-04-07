b7e38453213deb9c39cb728f017d8aa7a794c72e
mehyarbelal1@hotmail.com <mehyarbelal1@hotmail.com>
2021-04-07 20:42:43 +1000

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let products = document.querySelectorAll(".cards");
 
 products.forEach(function (item) {
   item.addEventListener("click", function () {
-    alert(item.childNodes[3].innerHTML);
+    let clickedItem = item.childNodes[3].innerHTML;
+
+    alert(clickedItem);
+    location.href = "productDetails.html";
   });
 });
